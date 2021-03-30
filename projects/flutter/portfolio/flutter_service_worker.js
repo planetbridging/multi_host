38fd61a174f3ecf40f85b3cf0cb3c6a13a8f8ce6
planetbridging <47130739+planetbridging@users.noreply.github.com>
2021-03-30 23:00:28 +1000

    Updated portfolio

--- projects/flutter/portfolio/flutter_service_worker.js
+++ projects/flutter/portfolio/flutter_service_worker.js
@@ -13,9 +13,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f1730802a723c2de8d35589eaaabbf71",
-"/": "f1730802a723c2de8d35589eaaabbf71",
-"main.dart.js": "33036992357617f4a3fcce21bf425658",
+"index.html": "29d82cb36de11b39f286dfece571c2e9",
+"/": "29d82cb36de11b39f286dfece571c2e9",
+"main.dart.js": "60b03919f50cbd65072624b4d09f35b4",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
 };

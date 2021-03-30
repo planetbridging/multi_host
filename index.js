cf64e265997d2c534478faf21a18888fa85ac910
planetbridging <47130739+planetbridging@users.noreply.github.com>
2021-03-30 13:36:42 +1000

    bedrock

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var subdomain = require("express-subdomain");
+var app = express();
+var router = express.Router(); //main api router
+var v1Routes = express.Router();
+var v2Routes = express.Router();
+
+v1Routes.get("/", function (req, res) {
+  res.send("API - version 1");
+});
+v2Routes.get("/", function (req, res) {
+  res.send("API - version 2");
+});
+
+var checkUser = function (req, res, next) {
+  if (!req.session.user.valid) {
+    return res.send("Permission denied.");
+  }
+  next();
+};
+
+//the api middleware flow
+router.use(checkUser);
+router.use(subdomain("*.v1", v1Routes));
+router.use(subdomain("*.v2", v2Routes));
+
+//basic routing..
+router.get("/", function (req, res) {
+  res.send("Welcome to the API!");
+});
+
+//attach the api
+app.use(subdomain("api", router));
+app.listen(9999);
